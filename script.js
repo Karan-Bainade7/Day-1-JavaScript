252f1aec581ecd39b545076f1fb34517ec703337
karan punamsing bainade <119426472+Karan-Bainade7@users.noreply.github.com>
2024-11-23 20:51:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+console.log("hello world karan")
+age = 22;
+name = "karan";
+console.log(age);
+console.log(name);
+
+
+
+
+isfollow = false;
+console.log(isfollow);
+// we can not be redecleard in et keywords
+let a = 10;
+let a = 20;
+console.log(a);
+
+
+
+
+
+let b = 20;
+b = 30;
+console.log(b);
+
+// we can define also in
+// var keywords redeclear and updated
+var a = 20;
+var a = 30;
+console.log(a);
+
+var a = 20;
+a = 30;
+console.log(a);
+
+// Datatype
+
+
+
+let a = 10;
+console.log(a);
+let c = "karan bainade"
+console.log(c);
+
+let d = true;
+console.log(d);
+
+let e;
+console.log(e)
+
+let age = null;
+console.log(age);
+
+
+const Karan = {
+    job: "Nothing ",
+    money: 0,
+    love: false,
+    success: 0.00,
+    ischangelife: true,
+}
+console.log(Karan["love"]);
